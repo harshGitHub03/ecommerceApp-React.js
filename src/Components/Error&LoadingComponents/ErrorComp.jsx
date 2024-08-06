0690d75b928784f980d903761851836d83d2a482
harshGit <harshcode07@gmail.com>
2024-08-06 20:52:46 +0530

    first commit

--- src/Components/Error&LoadingComponents/ErrorComp.jsx
+++ src/Components/Error&LoadingComponents/ErrorComp.jsx
@@ -9,7 +9,7 @@ function ErrorComp() {
             </div>
             <h4 className='text-3xl text-red-700 font-medium'>404 NOTHING FOUND</h4>
             <p className='text-lg text-center'>Looks Like Webpage you're looking for <br />dosent exist!</p>
-            <Link to='/' className='px-6 font-semibold rounded text-white hover:bg-[#16837b] active:bg-[#0c544f] py-2 bg-[#08726a]'>Sign Up</Link>
+            <Link to='/' className='px-6 font-semibold rounded text-white hover:bg-[#16837b] active:bg-[#0c544f] py-2 bg-[#08726a]'>home</Link>
           </div>
     )
 }
